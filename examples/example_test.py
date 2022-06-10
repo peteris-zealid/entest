fff171db1f55c3cf38c3bde5b0e6d5fed6b529ca
Peteris Ratnieks <peteris.ratnieks@zealid.com>
2022-06-10 15:15:20 +0300

    lint: black

--- examples/example_test.py
+++ examples/example_test.py
@@ -3,6 +3,7 @@ import examlpe_src.internals as src_internals
 
 from entest import STATUS, assert_raises, depends_on
 
+
 @depends_on()
 def create_user():
     user1 = src.create_user("my_name")
@@ -12,12 +13,14 @@ def create_user():
         "user_id2": user2["id"],
     }
 
+
 @depends_on()
 def create_spam(user_id1):
     spam = src.create_spam(user_id1, "500g")
     return {
         "spam_id": spam["id"],
     }
+
 
 @depends_on()
 def offer_spam(spam_id, user_id2):
@@ -28,11 +31,13 @@ def offer_spam(spam_id, user_id2):
 def deposit_money_in_bank(user_id2):
     src.deposit(user_id2, "100£")
 
+
 @depends_on(offer_spam, previous=True)
 def order_spam(user_id2, spam_id):
     src.order_spam(user_id2, spam_id)
     spam = src.internals.get_spam(spam_id)
     assert spam.owner == user_id2
+
 
 @depends_on(offer_spam, without=deposit_money_in_bank)
 def order_spam_insufficient_funds(user_id1, user_id2, spam_id):
@@ -43,11 +48,13 @@ def order_spam_insufficient_funds(user_id1, user_id2, spam_id):
     spam = src_internals.get_spam(spam_id)
     assert spam.owner == user_id1
 
+
 @depends_on(create_spam)
 def eat_spam(spam_id, user_id1, user_id2):
     user_id = src.eat_spam(spam_id)
     expected_user_id = user_id2 if order_spam.status == STATUS.passed else user_id1
     assert user_id == expected_user_id
+
 
 @depends_on(create_user, run_last=True)
 def delete_user(user_id1):
