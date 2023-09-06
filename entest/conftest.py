28b0fe74065eb9a79afe9d9e6388c76f933428fd
Peteris Ratnieks <peteris.ratnieks@zealid.com>
2023-09-06 12:14:58 +0300

    define conftest.py

---
+++ entest/conftest.py
@@ -0,0 +1,58 @@
+import random
+from collections import defaultdict
+from typing import List
+from sys import exit
+
+try:
+    from tests.conftest import Conftest as UserConftest
+except ImportError:
+    print("""
+        You need to define conftest.py in your tests/ folder.
+        The file should include a class Conftest that does not inherit anything.
+        You can use super() to access the default method definitions.
+        For example this is good enough:
+
+# tests/conftest.py
+class Conftest:
+    pass
+
+    """)
+    exit(5)
+
+
+class DefaultConftest:
+    RANDOM_SEED = 1
+    def __init__(self) -> None:
+        random.seed(self.RANDOM_SEED)
+
+    def get_testing_order(self, tests_to_be_run: List["TestCase"]):
+        """
+        Note that there is no requirement for the returned list to contain
+        all elements of the one passed as the argument.
+        """
+        order = []
+        unvisited = tests_to_be_run.copy()
+        danger_zone = defaultdict(int)
+        for test in filter(lambda test: test.without, unvisited):
+            danger_zone[test.without] += 1
+        while unvisited:
+            random.shuffle(unvisited)
+            start_len = len(unvisited)
+            for test in unvisited:
+                if all(parent in order for parent in test.parents) and danger_zone[test] <= 0:
+                    order.append(test)
+                    unvisited.remove(test)
+                    danger_zone[test.without] -= 1
+            if len(unvisited) == start_len:
+                raise Exception(
+                    "Impossible to traverse all graph nodes. Use --graph to review dependencies",
+                )
+        order.sort(key=lambda test_case: int(test_case.run_last))
+        return order
+
+
+class Conftest(UserConftest, DefaultConftest):
+    pass
+
+
+conftest = Conftest()
--- entest/runner.py
+++ entest/runner.py
@@ -1,57 +1,18 @@
-import random
 import threading
-from collections import defaultdict
 from os import abort
 from typing import Callable
 
 from entest.const import STATUS
+from entest.conftest import conftest
 from entest.dependency_decorator import TEST_ROOT, TestCase
 from entest.status_report import IS_STACKPRINTER, StatusPanel
 
 
-def breadth_first_traverse(root: TestCase = TEST_ROOT):
-    order = []
-    nodes = [root]
-    current_depth = 0
-    while nodes:
-        order.extend(nodes)
-        current_depth += 1
-        nodes = [test for test in TestCase.full_registry.values() if test.depth == current_depth]
-    order.sort(key=lambda test_case: int(test_case.run_last))
-    return order
-
-
-def bogo_order(seed: int = 0):
-    random.seed(seed)
-    order = []
-    unvisited = [test for test in TestCase.full_registry.values()]
-    danger_zone = defaultdict(int)
-    for test in filter(lambda test: test.without, unvisited):
-        danger_zone[test.without] += 1
-    while unvisited:
-        random.shuffle(unvisited)
-        start_len = len(unvisited)
-        for test in unvisited:
-            if all(parent in order for parent in test.parents) and danger_zone[test] <= 0:
-                order.append(test)
-                unvisited.remove(test)
-                danger_zone[test.without] -= 1
-        if len(unvisited) == start_len:
-            raise Exception(
-                "Impossible to traverse all graph nodes. Use --graph to review dependencies",
-            )
-    order.sort(key=lambda test_case: int(test_case.run_last))
-    return order
-
-
-def generate_run_sequence(*, strategy=bogo_order):
-    order = strategy()
-    return [tc for tc in order if tc.status == STATUS.wait]
-
-
 def run_tests(logger: Callable[..., None]):
     logger("""=============== Running tests ===============""")
-    test_order = generate_run_sequence()
+    tests_to_be_run = [tc for tc in TestCase.full_registry.values() if tc.status == STATUS.wait]
+    test_order = conftest.get_testing_order(tests_to_be_run)
+    # TODO check test_order is valid (because ordering can be defined by user)
     with StatusPanel(test_order, logger):
         for test in test_order:
             test()
